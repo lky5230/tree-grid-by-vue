1d9685963886ba2a4832f48daef8c16681e653d7
lky5230 <547859733@qq.com>
2017-11-11 15:06:16 +0800

    Update test.vue

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,88 +1,12 @@
-<template>
-  <div>
-    <tree-grid
-        :columns="columns"
-        :rowdata="data"
-        :needUpdate="needUpdate"
-        leafUrl="http://api.fanhua.dev.tianheng-uestc.com/api/v1/apitest//component/treegrid/base?parentid="
-        :treeLoading="treeLoading"
-
-        @refreshTable="refreshTable"
-        @uploadmodify="uploadmodify"
-        @uploaddelete="uploaddelete"
-        ></tree-grid>
-  </div>
-</template>
-
-<script>
-import treeGrid from '@/commonComponents/tree-grid'
-export default {
-  data(){
-    return {
-        columns: [
-            {name: 'ID', prop: 'id', width: 50},
-            {name: '删除',  delete: true }, //删除、操作不需要width
-            {name: 'name字段', prop: 'name', width: 260, isTree: true, edit: true},
-            {name: '操作',  operate: true }, //删除、操作不需要width
-            // {name: 'level', prop: 'level', width: 120},
-            {name: 'url', prop: 'url', edit: true},
-        ],
-        data: [],
-        needUpdate: Date.now(),
-
-        treeLoading: false,
-    }
-  },
-  created(){
-      this.treeLoading = true;
-      this.$http.get('static/menu.json').then(res=>{
-            setTimeout(()=>{
-                this.treeLoading = false;
-                this.data = res.data;
-                this.needUpdate = Date.now();
-            }, 1400);
-        });
-  },
-  methods: {
-      refreshTable(){
-        this.treeLoading = true;
-        this.$http.get('static/menu.json').then(res=>{
-            setTimeout(()=>{
-                this.treeLoading = false;
-                this.data = res.data;
-                this.needUpdate = Date.now();
-            }, 1400);
-        });
-      },
-      uploadmodify([data, successFn_callback, faildFn_callback]){
-            console.log(data)
-            setTimeout(()=>{
-                if(Math.random() > 0.1){
-                    // 上传成功
-                    successFn_callback()
-                }else{
-                    // 上传失败
-                    faildFn_callback()
-                }
-            }, 1100);
-      },
-      uploaddelete([deleteIdArray, successFn_callback, faildFn_callback]){
-            console.log(deleteIdArray)
-            setTimeout(()=>{
-                if(Math.random() > 0.1){
-                    // 删除成功
-                    successFn_callback()
-                }else{
-                    // 删除失败
-                    faildFn_callback()
-                }
-            }, 1100);
-      },
-
-  },
-  components: {
-      treeGrid
-  }
-}
-</script>
-
+<tree-grid 
+            :columns="columns" 
+            :rowdata="data" 
+            :needUpdate="needUpdate" 
+            :onlyAddOne="true"  //是否只能在存在的节点下增加下级
+            :leafUrl="leafUrl"  //异步获取子节点，以点击节点的id为get方法的参数
+            :treeLoading="treeLoading"
+            @currentDate="currentDate"  //监听每次树的变动
+            @refreshTable="refreshTable"   
+            @uploadmodify="uploadmodify" 
+            @uploaddelete="uploaddelete">
+        </tree-grid>
